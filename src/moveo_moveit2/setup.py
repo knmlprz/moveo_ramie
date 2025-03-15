c0bb2091fd170ce6226e44f66e85c431a5ae5760
FWalkowicz <fillio00@wp.pl>
2025-03-15 13:12:32 +0100

    movit init

---
+++ src/moveo_moveit2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from glob import glob
+package_name = 'moveo_moveit2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
+        ('share/' + package_name + '/config', glob('config/*.srdf')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='walkowiczf',
+    maintainer_email='fillio00@wp.pl',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
